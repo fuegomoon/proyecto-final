3390925b4d6ee8f5a52e8a36dc67092dcac15d61
Michelle <fuegomoon@yahoo.com>
2024-10-28 18:46:49 -0300

    changes to show current code, not funcioning,

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const LECTURE_LIST_KEY = "Clases del curso";
 
+
+
 function getLectureList() {
     let result = localStorage.getItem(LECTURE_LIST_KEY);
     if (result != null) {
@@ -12,12 +14,12 @@ function getLectureList() {
 }
 //json stringify +local storage.getItem
 function storeLectureList(lectureList) {
-    lecturesString = JSON.stringify(lectureList);
-    localStorage.setItem(LECTURE_LIST_KEY, lecturesString);
+    lectureString = JSON.stringify(lectureList);
+    localStorage.setItem(LECTURE_LIST_KEY, lectureString);
 }
 
 const lectureListEl = document.getElementById("lectureList");
-const lectureList = getLectureList();
+//const lectureList = getLectureList();
 
 const makeVideoThumbnailUrl = (lecture) => {
     const params = new URL(lecture.videoURL).searchParams;
@@ -28,29 +30,111 @@ const makeVideoThumbnailUrl = (lecture) => {
 }
 
 
-lectureList.forEach((lecture) => {
-    lectureListEl.insertAdjacentHTML(
-        "beforeend",
-        `
-        <div id="${lecture.id}" class= 'lecture'>
-            <h2>${lecture.title}</h2>
-            <a href="${lecture.videoURL}">
-                <img src="${makeVideoThumbnailUrl(lecture)}" class= "thumbnailURL" target= "_blank">
-            </a>
-        </div>
 
-        `
-    );
-   
-    // const lectureEl = document.getElementById(`#${lecture.id}`);
-    // lectureEl.addEventListener('click', showClassDetails);
+const addClassDialogEl = document.getElementById("js-addClassDialog");
+//show the dialog box
+document.querySelector(".js-addClassBtn").addEventListener('click', () => {
+    const newState = changeFunction(window.state, { type: "OPEN_DIALOG" });
+    render(newState);
+    //new code :
+    //const action = {type: "OPEN_DIALOG"};
+    //store.dispatch(action);
 });
 
+//receive event of form data and add it to the page
+const addClassFormEl = document.getElementById("js-addClassForm");
 
+addClassFormEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formDataEntries = Object.fromEntries(formData.entries());
+    const newLecture = newLectureFromFormDataEntries(formDataEntries);
+    addClassFormEl.reset();
+    const action = {type: "ADD_CLASS", payload: newLecture};
+    const newState = changeFunction(window.state, action);
+    render(newState);
+    //instead of last two lines:
+    //store.dispatch(action);
+});
 
-// Sumá la clase a la lista de las clases que ya existen con .push({ title: title, completed: false }) y llamá a storeClassesList
-document.querySelector(".button").addEventListener('click', () => {
-    const title = prompt("Enter the title of the class");
+const newLectureFromFormDataEntries = (formDataEntries) => {
+    const result = {};
+
+    result.id= window.state.lectureList.length + 1;
+    //replace last line with
+    //result.id = store.getState().lectureList.length + 1;
+
+    if (formDataEntries.isCompleted === "on") { 
+        result.isCompleted = true;
+    } else {
+        result.isCompleted = false;
+    }
+    if (formDataEntries.isHomeworkDone === "on") { 
+        result.isHomeworkDone = true;
+    } else {
+        result.isHomeworkDone = false;
+    }
+    result.title= formDataEntries.title;
+    result.videoURL= formDataEntries.videoURL;
+    result.videoThumbnailUrl = formDataEntries.videoThumbnailUrl;
+    return result;
+}
+
+//STATE
+const initialState = () => {
+    return {
+        lectureList: getLectureList(),
+        isDialogOpen: false,
+    };
+};
+const changeFunction = (currentState, action) => {
+    if (typeof currentState === "undefined") {
+        return initialState();
+    }
+    const newState = {...currentState};
+    if (action.type === "OPEN_DIALOG") {
+        newState.isDialogOpen = true;
+        return newState;
+    } else if (action.type === "ADD_CLASS") {
+        newState.lectureList.push(action.payload);
+        newState.isDialogOpen = false;
+        return newState;
+    }
+    return newState;
+};
+//const store = Redux.createStore(changeFunction);
+const logState = (state) => {
+    console.log("state",state);
+};
+const renderDialog = (isDialogOpen) => {
+    if (isDialogOpen) { 
+        addClassDialogEl.showModal();
+    } else {
+        addClassDialogEl.close();
+    };
+}    
+
+const renderLectureList = (lectureList) => {
+    lectureListEl.innerHTML = "";
+    lectureList.forEach((lecture) => {
+        lectureListEl.innerHTML += 
+            `
+            <div id="${lecture.id}" class= 'lecture'>
+                <h2>${lecture.title}</h2>
+                <a href="${lecture.videoURL}">
+                    <img src="${makeVideoThumbnailUrl(lecture)}" class= "thumbnailURL" target= "_blank">
+                </a>
+            </div>
     
-   // storeLectureList
-});
+            `;
+    });
+};
+
+const render = (state) => {
+    window.state = state;
+    storeLectureList(state.lectureList);
+    logState(state);
+    renderDialog(state.isDialogOpen);
+    renderLectureList(state.lectureList);
+}
+render(initialState()); 
